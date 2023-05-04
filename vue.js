bc25723fde9dcec2b27a901243c9091bba5561b2
diego londoño <diegoferlondo13@gmail.com>
2023-05-04 18:08:34 -0500

    uso del v-for con doble interpolación {{}} {{}} además de mostrar los valores que acompañan a cada variable declarada y que se encuentra en el archivo .js

--- vue.js
+++ vue.js
@@ -2,7 +2,17 @@ new Vue({
     el:"#vm",
     data: {
         
-        paises: ['Colombia', 'EEUU', 'España', 'Londres' ]
+        empleado:{
+            Nombre: 'Diego Londoño',
+            Edad: '41 años',
+            Nacionalidad: 'Colombiano',
+            Profesión: 'Desarrollador',
+            Celular: '301009384857'
+
+
+        }
+        
+        //paises: ['Colombia', 'EEUU', 'España', 'Londres' ]
         
         //mostrar: false
         
